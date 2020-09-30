c33ba5afd4409c57f90e215ad39f9ccb7f909ae8
vhitech <abi666ht@gmail.com>
2020-09-30 21:13:11 +0530

    crud operations completed

--- src/pages/employee/Form.js
+++ src/pages/employee/Form.js
@@ -12,106 +12,105 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import DialogContentText from '@material-ui/core/DialogContentText';
 
 const initialState = {
-    id: '',
-    firstname: '',
-    lastname: '',
-    skills: []
+  firstname: '',
+  lastname: '',
+  skills: []
 }
 
 const EmployeeForm = (props) => {
-    const { handleClose, selectedId, throwAlert } = props;
-    const { loading, error, data } = useQuery(gql(listSkills));
-    const [state, setState] = React.useState(initialState);
-    const skills = data.listSkills.items;
+  const { handleClose, selectedId, throwAlert, delEmployee, addOrUpdateEmp } = props;
+  const { loading, error, data } = useQuery(gql(listSkills));
+  const [state, setState] = React.useState(initialState);
+  const { data: empData } = useQuery(gql(getEmployee), {
+    variables: {
+      id: selectedId,
+    },
+    skip: !selectedId
+  });
 
-    const { data: empData } = useQuery(gql(getEmployee), {
-        variables: {
-            id: selectedId,
-        },
-        skip: !selectedId
-    });
-
-    React.useEffect(() => {
-        if (empData.getEmployee) {
-            const { id, firstname, lastname, skills } = empData.getEmployee
-            setState({ id, firstname, lastname, skills })
-        }
-    }, [empData])
-
-    if (loading) return '';
-    if (error) return `Error! ${error.message}`;
-
-    const onDialogClose = () => {
-        setState(initialState)
-        handleClose()
+  React.useEffect(() => {
+    console.log(selectedId)
+    if (empData.getEmployee) {
+      const { id, firstname, lastname, skills } = empData.getEmployee
+      setState({ id, firstname, lastname, skills: skills.map(({ id, name }) => ({ id, name })) })
     }
-    const renderTextField = (label, key) => (
-        <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label={label}
-            onChange={(event) => {
-                const val = { ...state }
-                val[key] = event.target.value
-                setState(val)
-            }}
-            value={state[key]}
-            fullWidth
-        />
-    );
-    const renderFormContent = () => (
-        <DialogContent>
-            {renderTextField("First Name", 'firstname')}
-            {renderTextField("Last Name", 'lastname')}
-            <Autocomplete
-                multiple
-                id="tags-standard"
-                options={skills}
-                getOptionLabel={(option) => option.name}
-                onChange={(event, value) => setState({ ...state, skills: value })}
-                value={state.skills}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        variant="standard"
-                        label="Skills"
-                    />
-                )}
-            />
+  }, [empData, data])
 
-        </DialogContent>
-    )
-    const renderAlert = () => (
-        <DialogContent>
-            <DialogContentText id="alert-dialog-slide-description">
-                Are you sure you want to  delete this employee?
-            </DialogContentText>
-        </DialogContent>
-    )
+  if (loading) return '';
+  if (error) return `Error! ${error.message}`;
 
-    return (
-        <Dialog open={true} onClose={onDialogClose} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">
-               {throwAlert ?'Delete' : 'Employee Details'}
-            </DialogTitle>
-            {throwAlert ? renderAlert() : renderFormContent()}
+  const skills = data.listSkills.items.map(({ id, name }) => ({ id, name }))
 
-            <DialogActions>
-                <Button onClick={onDialogClose} color="primary">
-                    Cancel
-                </Button>
-                {throwAlert ?
-                    <Button onClick={onDialogClose} color="primary">
-                        Delete
-                    </Button> :
-                    <Button onClick={onDialogClose} color="primary">
-                        {selectedId ? 'Update' : 'Create'}
-                    </Button>
-                }
-            </DialogActions>
-        </Dialog>
-    )
+  const onDialogClose = () => {
+    setState(initialState)
+    handleClose()
+  }
+  const renderTextField = (label, key) => (
+    <TextField
+      autoFocus
+      margin="dense"
+      label={label}
+      onChange={(event) => {
+        const val = { ...state }
+        val[key] = event.target.value
+        setState(val)
+      }}
+      value={state[key]}
+      fullWidth
+    />
+  );
+  const renderFormContent = () => (
+    <DialogContent>
+      {renderTextField("First Name", 'firstname')}
+      {renderTextField("Last Name", 'lastname')}
+      <Autocomplete
+        multiple
+        id="tags-standard"
+        options={skills}
+        getOptionLabel={(option) => option.name}
+        onChange={(event, value) => setState({ ...state, skills: value })}
+        value={state.skills}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            label="Skills"
+          />
+        )}
+      />
+
+    </DialogContent>
+  )
+  const renderAlert = () => (
+    <DialogContent>
+      <DialogContentText id="alert-dialog-slide-description">
+        Are you sure you want to  delete this employee?
+      </DialogContentText>
+    </DialogContent>
+  )
+
+  return (
+    <Dialog open={true} onClose={onDialogClose} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">
+        {throwAlert ? 'Delete' : 'Employee Details'}
+      </DialogTitle>
+      {throwAlert ? renderAlert() : renderFormContent()}
+
+      <DialogActions>
+        <Button onClick={onDialogClose} color="primary">
+          Cancel
+         </Button>
+        {throwAlert ?
+          <Button onClick={delEmployee} color="primary">
+            Delete
+           </Button> :
+          <Button onClick={() => addOrUpdateEmp(state)} color="primary">
+            {selectedId !== '0' ? 'Update' : 'Create'}
+          </Button>
+        }
+      </DialogActions>
+    </Dialog>
+  )
 };
 
 export default EmployeeForm
--- src/pages/employee/List.js
+++ src/pages/employee/List.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Table from '../../components/Table'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createEmployee, updateEmployee, deleteEmployee } from '../../graphql/mutations'
-import { listEmployees, listSkills } from '../../graphql/queries'
+import { listEmployees } from '../../graphql/queries'
 import gql from 'graphql-tag'
-import { useQuery } from "react-apollo-hooks";
+import { useQuery, useMutation } from "react-apollo-hooks";
 import EmployeeForm from './Form'
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import { Grid } from "@material-ui/core";
 
 const tableHeader = () => {
     return [
@@ -15,52 +18,91 @@ const tableHeader = () => {
     ];
 };
 
+
 const EmployeeList = () => {
     const [state, setState] = React.useState({ open: false, selectedId: '0', throwAlert: false });
     const { open, selectedId, throwAlert } = state
-    const { loading, error, data } = useQuery(gql(listEmployees));
+    const { loading, error, data, refetch } = useQuery(gql(listEmployees));
+    const [employees, setEmployeeList] = React.useState([])
+    const deleteEmp = useMutation(gql(deleteEmployee));
+    const addEmp = useMutation(gql(createEmployee));
+    const updateEmp = useMutation(gql(updateEmployee));
+
+    React.useEffect(() => {
+        if (data.listEmployees) {
+            data.listEmployees.items.map((emp) => {
+                emp.fullname = `${emp.firstname} ${emp.lastname}`
+                emp.skillset = emp.skills.map((skills) => skills.name)
+            })
+            setEmployeeList(data.listEmployees.items)
+        }
+    }, [data])
 
     if (loading) return 'Loading...';
     if (error) return `Error! ${error.message}`;
 
-    const employees = data.listEmployees.items;
-    employees.map((emp) => {
-        emp.fullname = `${emp.firstname} ${emp.lastname}`
-        emp.skillset = emp.skills.map((skills) => skills.name)
-    })
 
-    const handleClickOpen = (emp) => {
-        console.log(emp)
-        setState({ open: true, selectedId: emp.id })
+
+    const handleClickOpen = (emp, alert) => {
+        setState({ open: true, selectedId: emp.id, throwAlert: alert || false })
     };
-
+    const delEmployee = async () => {
+        await deleteEmp({
+            variables: {
+                input: { id: selectedId },
+            }
+        })
+        refetch()
+        handleClose()
+    }
+    const addOrUpdateEmp =async (input) => {
+        if (input.id) {
+            await updateEmp({ variables: { input } })
+        } else {
+            await addEmp({ variables: { input } })
+        }
+        refetch()
+        handleClose()
+    }
     const handleClose = () => {
-        setState({ open: false, selectedId: '0',throwAlert: false });
+        setState({ open: false, selectedId: '0', throwAlert: false });
     };
     const tableBody = () => {
-        return <div style={{ width: '80%' }}>
+        return <Grid
+            spacing={2}
+            container
+            justify={'flex-end'}
+        >
+            <Grid item >
+                <Button variant="contained" color="primary"
+                    onClick={() => setState({ ...state, open: true })}>
+                    Add Employee
+             </Button>
+            </Grid>
+
             <Table
                 headers={tableHeader()}
                 rows={employees}
                 actions={[
                     { name: 'Edit', action: handleClickOpen },
-                    { name: 'Delete', action: () => setState({ ...state, open:true,throwAlert: true }) }
+                    { name: 'Delete', action: (emp) => handleClickOpen(emp, 'THROW_ALERT') }
                 ]}
             />
-        </div>
+        </Grid>
+
 
     }
     return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Container fixed>
             {tableBody()}
             {open && <EmployeeForm
                 handleClose={handleClose}
                 selectedId={selectedId}
                 throwAlert={throwAlert}
+                delEmployee={delEmployee}
+                addOrUpdateEmp={addOrUpdateEmp}
             />}
-
-
-        </div>
+        </Container>
     )
 }
 
